e0f9aff70258e6fc6a3ad9523819d2b46b588d13
at-minhtang <minh.tang@asiantech.vn>
2019-05-31 20:43:40 +0700

    update logic render component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
   def component(component_name, locals = {}, &block)
-    name = component_name.split('_').first
-    render("components/#{name}/#{component_name}", locals, &block)
+    if locals.key?(:collection)
+      partial = component_name.singularize
+      partial = locals[:partial] if locals[:partial].present?
+      path = "components/#{component_name}/#{partial}"
+      render partial: path, collection: locals[:collection], as: locals[:as], locals: locals[:locals]
+    else
+      partial = component_name.split('/').last
+      render("components/#{component_name}/#{partial}", locals, &block)
+    end
   end
 
   alias c component
