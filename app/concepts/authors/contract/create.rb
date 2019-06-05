50eac737527aaa21f909d57c09607203c67f2a9b
at-minhtang <minh.tang@asiantech.vn>
2019-06-05 16:08:19 +0700

    create new user

--- app/concepts/authors/contract/create.rb
+++ app/concepts/authors/contract/create.rb
@@ -6,7 +6,7 @@ module Authors::Contract
 
     validates :fullname, :email, :age, presence: true
     validates :fullname, :email, length: { maximum: 255 }
-
+    validates :email, email: true, unique: { case_sensitive: false }
     validates :age, numericality: { only_integer: true, greater_than: 16, less_than: 200 }
   end
 end
